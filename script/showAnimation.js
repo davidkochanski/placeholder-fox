796552b8e452468c159535c3e75cd2879774b759
David Kochanski <105320855+davefoxxo@users.noreply.github.com>
2023-03-13 21:51:57 -0400

    Fox demo works

--- script/showAnimation.js
+++ script/showAnimation.js
@@ -1,5 +1,7 @@
 document.onmousemove = e => {
     const emptyFox = document.getElementById("empty-fox");
+    const emptyHeader = document.querySelector(".hero .hero-content.empty-content");
+    const fullHeader = document.querySelector(".hero .hero-content.full-content");
 
     let x = e.clientX;
     let width = window.innerWidth;
@@ -7,4 +9,6 @@ document.onmousemove = e => {
     let percent = 100 * x / width
 
     emptyFox.style.clipPath = `inset(0 ${100 - percent}% 0 100%)`;
+    fullHeader.style.clipPath = `inset(0 100% 0 ${percent}% )`;
+    emptyHeader.style.clipPath = `inset(0 ${100 -percent}% 0 100%)`;
 }
