17d08b703544044f74e3b22bb2738351ec09c369
davefoxxo <kochanskidavid9@gmail.com>
2023-03-24 11:09:14 -0400

    Update domain

--- script/randomFox.js
+++ script/randomFox.js
@@ -1,14 +1,28 @@
 const button = document.getElementById("new-fox");
 const randomUrl = document.getElementById("random-code");
+const randomUrlLink = document.querySelector(".random-url > a");
 const foxImage = document.getElementById("random-fox-image");
+let cooldown = false;
 
 button.onmousedown = () => {
+    if(cooldown) return;
+
+    cooldown = true;
+
     let [ newFox, newFoxUrl ] = getNewFox();
 
     randomUrl.innerHTML = newFox;
+    randomUrlLink.href = newFoxUrl;
 
-    // foxImage.src = newFoxUrl;
+    foxImage.src = newFoxUrl;
 
+    button.style.opacity = 0.33;
+
+    setTimeout(() => {
+        cooldown = false; 
+        button.style.opacity = 1;
+
+    }, 1333);
 }
 
 const randomFromRange = (min, max) => {
@@ -19,11 +33,12 @@ const types = ["red", "arctic", "fennec"];
 const filters = ["", "", "", "-bw", "-one"]
 
 const getNewFox = () => {
+    // Wildcard chance
     if(!randomFromRange(0, 10)) {
         num = randomFromRange(0,16)
 
-        return [`https://<span class="n">placefox.com</span>/<span class="x">${num}</span>.jpg`,
-                `https://placefox.com/${num}.jpg`]
+        return [`https://<span class="n">placefox.it</span>/<span class="x">${num}</span>.jpg`,
+                `https://placefox.it/${num}.jpg`]
     }
     const width = 10 * randomFromRange(10, 100);
     const height = 10 * randomFromRange(10, 100);
@@ -32,6 +47,6 @@ const getNewFox = () => {
     const type = types[randomFromRange(0, types.length)];
     const filter = filters[randomFromRange(0, filters.length)];
 
-    return [`https://<span class="n">placefox.com</span>/<span class="w">${width}</span>/<span class="h">${height}</span>/<span class="t">${type}</span><span class="c">${filter}</span>.jpg`,
-            `https://placefox.com//${width}/${height}/${type}${filter}.jpg`];
+    return [`https://<span class="n">placefox.it</span>/<span class="w">${width}</span>/<span class="h">${height}</span>/<span class="t">${type}</span><span class="c">${filter}</span>.jpg`,
+            `https://placefox.it//${width}/${height}/${type}${filter}.jpg`];
 }
